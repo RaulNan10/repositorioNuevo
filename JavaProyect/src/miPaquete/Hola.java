e701bcacbe453e48e4cf4abe0362bfa679bc3c91
jnan <jnan@lmde>
2021-12-02 20:19:59 +0100

    commit 3

--- JavaProyect/src/miPaquete/Hola.java
+++ JavaProyect/src/miPaquete/Hola.java
@@ -3,5 +3,18 @@ package miPaquete;
 public class Hola {
 
 	String nombre = "hola";
+
+	public Hola(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	
+	public void saluda() {
+		
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
 	
 }
